c4cd9a4bafaf44a51f57800687e736682fe580f2
Régis GRARE <regisgrare@hotmail.fr>
2022-02-28 14:16:21 +0100

    homePage + FilterScreen + localStorage Pseudo

--- screens/homefilterScreen.js
+++ screens/homefilterScreen.js
@@ -1,6 +1,12 @@
 import { StyleSheet, View } from 'react-native';
-import { Button, Text, CheckBox } from 'react-native-elements';
+import { Button, Text, CheckBox, Icon } from 'react-native-elements';
+import { TouchableOpacity } from 'react-native';
 import { useState } from 'react';
+import {
+  PressStart2P_400Regular
+} from '@expo-google-fonts/press-start-2p';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 
 export default function homefilterScreen(props) {
@@ -8,35 +14,54 @@ export default function homefilterScreen(props) {
   const [check2, setCheck2] = useState(false);
   const [check3, setCheck3] = useState(false);
   const [check4, setCheck4] = useState(false);
+
+  let [fontLoaded, error] = useFonts({ PressStart2P_400Regular });
+
+  if (!fontLoaded) {
+    return <AppLoading />
+  }
   return (
 
     <View style={styles.container}>
-      <Text style={{ color: "#D1D8E0" }}>Quel genre de lieu souhaitez-vous Veaziter ?</Text>
+      <Text style={{ color: "#06D4B6", fontSize: 15, fontFamily: "PressStart2P_400Regular", marginBottom: 80 }}>Quel(s) genre(s) de lieu souhaitez-vous Veaziter ?</Text>
+
       <CheckBox
         center
-        title="LIEUX CULTURELS"
+        title="Click Here"
         checked={check1}
+        checkedColor="#06D4B6"
         onPress={() => setCheck1(!check1)}
       />
 
       <CheckBox
         center
-        title="LA BONNE BOUFFE"
+        title="Click Here"
         checked={check2}
+        checkedColor="#06D4B6"
         onPress={() => setCheck2(!check2)}
       />
+
       <CheckBox
         center
-        title="MICKEY"
+        title="Click Here"
         checked={check3}
+        checkedColor="#06D4B6"
         onPress={() => setCheck3(!check3)}
       />
+
       <CheckBox
         center
-        title="PLUTO"
+        title="Click Here"
         checked={check4}
+        checkedColor="#06D4B6"
         onPress={() => setCheck4(!check4)}
       />
+
+      <TouchableOpacity onPress={() => props.navigation.navigate('StackNavigation')}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>GO!</Text>
+        </View>
+      </TouchableOpacity>
 
     </View>
 
@@ -49,5 +74,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#2C3A47',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: "#2C3A47",
+    borderWidth: 1,
+    borderColor: "#06D4B6",
+    padding: 15,
+    paddingTop: 25,
+    borderRadius: 30,
+    marginTop: 60
+
+  },
+  buttonText: {
+    color: "#06D4B6",
+    fontSize: 20,
+    fontFamily: "PressStart2P_400Regular"
   },
 });
